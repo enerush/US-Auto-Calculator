11d38ca87acd571605976a31dead1968f3cd7497
enerush <nyw077@gmail.com>
2022-07-29 17:32:39 +0300

    pull to db

---
+++ pull_xlsx_to_db.py
@@ -0,0 +1,25 @@
+import sqlite3
+from openpyxl.reader.excel import load_workbook
+from datetime import date
+
+
+def pull_to_db(xlsx_file: str):
+    book = load_workbook(xlsx_file)
+    sheet = book.active
+    rows = sheet.rows
+
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()
+
+    for row in rows:
+        auction, city, seaport, price = row
+        cursor.execute(f'''INSERT INTO calculator_city(
+                        auction, city, seaport, price, last_update) 
+                        VALUES 
+                        ("{auction.value}", "{city.value}", "{seaport.value}", "{price.value}", "{date.today()}")''')
+
+    conn.commit()
+    conn.close()
+    
+
+pull_to_db('prices.xlsx')
